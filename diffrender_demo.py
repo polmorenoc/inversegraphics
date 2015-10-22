Fixed bug with my spherical harmonic rotation lines. Some other changes with ground truth, demo etc.

diffrender_demo.py
@@ -38,7 +38,7 @@ unpackSceneFromBlender = False
 loadSavedSH = False
 useGTasBackground = False
 refreshWhileMinimizing = False
-computePerformance = True
+computePerformance = False
 glModes = ['glfw','mesa']
 glMode = glModes[0]
 sphericalMap = False
@@ -170,7 +170,7 @@ else:
     envMapCoeffs = light_probes.getEnvironmentMapCoefficients(envMapTexture, envMapMean, 0, 'equirectangular')
 
 rotation = ch.Ch([0.0])
-phiOffsetGT = rotation
+phiOffsetGT = 0
 phiOffset = ch.Ch([0])
 
 chObjAzGT = rotation
@@ -182,10 +182,9 @@ chDistGT = ch.Ch([camDistance])
 totalOffsetGT = phiOffsetGT + chObjAzGT
 totalOffset = phiOffset
 
-chAmbientIntensityGT = ch.Ch([0.0])
-
-shCoeffsRGBGT = ch.Ch(np.dot(envMapCoeffs.T,light_probes.sphericalHarmonicsZRotation(-totalOffsetGT)).T[:])
-shCoeffsRGBGTRel = ch.dot(envMapCoeffs.T,light_probes.sphericalHarmonicsZRotation(phiOffsetGT)).T
+chAmbientIntensityGT = ch.Ch([1.0])
+shCoeffsRGBGT = ch.Ch(np.dot(light_probes.sphericalHarmonicsZRotation(totalOffsetGT.r[:]), envMapCoeffs[[0,3,2,1,4,5,6,7,8]])[[0,3,2,1,4,5,6,7,8]])
+shCoeffsRGBGTRel = ch.Ch(np.dot(light_probes.sphericalHarmonicsZRotation(phiOffset.r[:]), envMapCoeffs[[0,3,2,1,4,5,6,7,8]])[[0,3,2,1,4,5,6,7,8]])
 
 chShCoeffsGT = 0.3*shCoeffsRGBGT[:,0] + 0.59*shCoeffsRGBGT[:,1] + 0.11*shCoeffsRGBGT[:,2]
 chShCoeffsGTRel = 0.3*shCoeffsRGBGTRel[:,0] + 0.59*shCoeffsRGBGTRel[:,1] + 0.11*shCoeffsRGBGTRel[:,2]
@@ -194,13 +193,14 @@ chAmbientSHGTRel = chShCoeffsGTRel.ravel() * chAmbientIntensityGT * clampedCosCo
 
 chLightRadGT = ch.Ch([0.1])
 chLightDistGT = ch.Ch([0.5])
-chLightIntensityGT = ch.Ch([5])
+chLightIntensityGT = ch.Ch([0])
 chLightAzGT = ch.Ch([0])
 chLightElGT = ch.Ch([0])
 angleGT = ch.arcsin(chLightRadGT/chLightDistGT)
 zGT = chZonalHarmonics(angleGT)
-shDirLightGTOriginal = chZonalToSphericalHarmonics(zGT, np.pi/2 - chLightElGT, chLightAzGT - np.pi/2)
-shDirLightGT = ch.Ch(shDirLightGTOriginal.r[:].copy())
+shDirLightGTOriginal = np.array(chZonalToSphericalHarmonics(zGT, np.pi/2 - chLightElGT, chLightAzGT - np.pi/2).r[:]).copy()
+shDirLightGT = ch.Ch(shDirLightGTOriginal.copy())
+chComponentGTOriginal = ch.array(np.array(chAmbientSHGT + shDirLightGT*chLightIntensityGT * clampedCosCoeffs).copy())
 chComponentGT = chAmbientSHGT + shDirLightGT*chLightIntensityGT * clampedCosCoeffs
 
 chAz = ch.Ch([0])
@@ -803,12 +803,22 @@ def readKeys(window, key, scancode, action, mods):
     if mods==glfw.MOD_SHIFT and key == glfw.KEY_RIGHT and action == glfw.RELEASE:
         refresh = True
         # chAz[0] = chAz[0].r + radians(1)
-        rotation[:] = rotation.r[0] + 0.1
-        shCoeffsRGBGT[:] = np.dot(envMapCoeffs.T,light_probes.sphericalHarmonicsZRotation(-totalOffsetGT.r[:])).T[:]
+        rotation[:] = rotation.r[0] + np.pi/4
+        # rotation[:] = np.pi/2
+        shCoeffsRGBGT[:] = np.dot(light_probes.sphericalHarmonicsZRotation(totalOffsetGT.r[:]), envMapCoeffs[[0,3,2,1,4,5,6,7,8]])[[0,3,2,1,4,5,6,7,8]]
         chAzGT[:] = rotation.r[:]
-        shDirLightGT[:] = np.dot(shDirLightGTOriginal.T,light_probes.sphericalHarmonicsZRotation(rotation)).T[:]
+        # ipdb.set_trace()
+        shOriginal = chComponentGTOriginal[[0,3,2,1,4,5,6,7,8]]
+        shOriginalDir = shDirLightGTOriginal[[0,3,2,1,4,5,6,7,8]]
+        # chComponentGT[:] = np.dot(light_probes.sphericalHarmonicsZRotation(rotation), shOriginal)[[0,3,2,1,4,5,6,7,8]]
+        # shDirLightGT[:] = np.dot(light_probes.sphericalHarmonicsZRotation(rotation), shOriginalDir)[[0,3,2,1,4,5,6,7,8]]
+        # shDirLightGT[:] = np.dot(shDirLightGTOriginal.T, light_probes.sphericalHarmonicsZRotation(rotation)).T[:]
+        # shDirLightGT[:] = np.sum(np.array(light_probes.sphericalHarmonicsZRotation(rotation) * shDirLightGTOriginal[:,None]), axis=1)
+        # shDirLightGT[:] = chZonalToSphericalHarmonics(zGT, np.pi/2 - chLightElGT, chLightAzGT + rotation[:] - np.pi/2).r[:]
+        print("Original: " + str(shDirLightGTOriginal))
         print(str(shCoeffsRGBGT.r))
         print(str(shDirLightGT.r))
+        print(str(rendererGT.tn[0]))
     if mods==glfw.MOD_SHIFT and key == glfw.KEY_DOWN and action == glfw.RELEASE:
         refresh = True
         chEl[0] = chEl[0].r - radians(1)

diffrender_groundtruth.py
@@ -229,7 +229,7 @@ def imageGT():
 # Initialization ends here
 #########################################
 
-prefix = 'train2'
+prefix = 'train1'
 
 print("Creating Ground Truth")
 trainSize = 50000
@@ -260,7 +260,7 @@ print("Generating renders")
 
 replaceableScenesFile = '../databaseFull/fields/scene_replaceables_backup.txt'
 sceneLines = [line.strip() for line in open(replaceableScenesFile)]
-scenesToRender = range(len(sceneLines))[0:10]
+scenesToRender = range(len(sceneLines))[:]
 lenScenes = 0
 for sceneIdx in scenesToRender:
     sceneNumber, sceneFileName, instances, roomName, roomInstanceNum, targetIndices, targetPositions = scene_io_utils.getSceneInformation(sceneIdx, replaceableScenesFile)
@@ -281,7 +281,7 @@ renderTeapotsList = np.arange(len(teapots))[0:1]
 #     if hdri[0] == 'data/hdr/dataset/canada_montreal_nad_photorealism.exr':
 #         hdrtorenderi = hdrit
 
-hdrstorender = list(envMapDic.items())[0:50]
+hdrstorender = list(envMapDic.items())[:]
 
 gtDtype = [('trainIds', trainIds.dtype.name), ('trainAzsGT', trainAzsGT.dtype.name),('trainObjAzsGT', trainObjAzsGT.dtype.name),('trainElevsGT', trainElevsGT.dtype.name),('trainLightAzsGT', trainLightAzsGT.dtype.name),('trainLightElevsGT', trainLightElevsGT.dtype.name),('trainLightIntensitiesGT', trainLightIntensitiesGT.dtype.name),('trainVColorGT', trainVColorGT.dtype.name, (3,) ),('trainScenes', trainScenes.dtype.name),('trainTeapotIds', trainTeapotIds.dtype.name),('trainEnvMaps', trainEnvMaps.dtype.name),('trainOcclusions', trainOcclusions.dtype.name),('trainTargetIndices', trainTargetIndices.dtype.name), ('trainComponentsGT', trainComponentsGT.dtype, (9,)),('trainComponentsGTRel', trainComponentsGTRel.dtype, (9,)), ('trainEnvMapPhiOffsets', trainEnvMapPhiOffsets.dtype)]
 
@@ -359,16 +359,15 @@ for sceneIdx in scenesToRender:
                     objAzGT = np.random.uniform(0,1)*(collisions[targetIndex][1][objAzInterval][1] - collisions[targetIndex][1][objAzInterval][0]) + collisions[targetIndex][1][objAzInterval][0]
                     objAzGT = objAzGT*np.pi/180
                     chObjAzGT[:] = objAzGT.copy()
-
                     totalOffset = phiOffset + chObjAzGT.r
-                    envMapCoeffsRotated = np.dot(envMapCoeffs.T,light_probes.sphericalHarmonicsZRotation(totalOffset)).T.copy()
-                    envMapCoeffsRotatedRel = np.dot(envMapCoeffs.T,light_probes.sphericalHarmonicsZRotation(phiOffset)).T.copy()
+                    envMapCoeffsRotated = np.dot(light_probes.sphericalHarmonicsZRotation(totalOffset), envMapCoeffs[[0,3,2,1,4,5,6,7,8]])[[0,3,2,1,4,5,6,7,8]].copy()
+                    envMapCoeffsRotatedRel = np.dot(light_probes.sphericalHarmonicsZRotation(phiOffset), envMapCoeffs[[0,3,2,1,4,5,6,7,8]])[[0,3,2,1,4,5,6,7,8]].copy()
                     shCoeffsRGB = envMapCoeffsRotated.copy()
                     shCoeffsRGBRel = envMapCoeffsRotatedRel.copy()
                     chShCoeffs = 0.3*shCoeffsRGB[:,0] + 0.59*shCoeffsRGB[:,1] + 0.11*shCoeffsRGB[:,2]
                     chShCoeffsRel = 0.3*shCoeffsRGBRel[:,0] + 0.59*shCoeffsRGBRel[:,1] + 0.11*shCoeffsRGBRel[:,2]
-                    chAmbientSHGT = chShCoeffs.ravel() * chAmbientIntensityGT * clampedCosCoeffs
-                    chAmbientSHGTRel = chShCoeffsRel.ravel() * chAmbientIntensityGT * clampedCosCoeffs
+                    chAmbientSHGT = chShCoeffs * chAmbientIntensityGT * clampedCosCoeffs
+                    chAmbientSHGTRel = chShCoeffsRel * chAmbientIntensityGT * clampedCosCoeffs
                     chComponentGT[:] = chAmbientSHGT.r[:].copy()
                     chComponentGTRel[:] = chAmbientSHGTRel.r[:].copy()
 
@@ -414,7 +413,6 @@ for sceneIdx in scenesToRender:
                         train_i = train_i + 1
 
             removeObjectData(0, v, f_list, vc, vn, uv, haveTextures_list, textures_list)
-
 
 # np.savetxt(gtDir + 'data.txt',np.array(np.hstack([trainIds[:,None], trainAzsGT[:,None], trainObjAzsGT[:,None], trainElevsGT[:,None], phiOffsets[:,None], trainOcclusions[:,None]])), fmt="%g")
 gtDataFile.close()

diffrender_test.py
@@ -35,7 +35,7 @@ plt.ion()
 glModes = ['glfw','mesa']
 glMode = glModes[0]
 
-width, height = (100, 100)
+width, height = (150, 150)
 win = -1
 
 if glMode == 'glfw':
@@ -198,10 +198,10 @@ methods=['dogleg', 'minimize', 'BFGS', 'L-BFGS-B', 'Nelder-Mead']
 seed = 1
 np.random.seed(seed)
 
-testPrefix = 'perfect2'
+testPrefix = 'train3'
 
-gtPrefix = 'test'
-trainPrefix = 'train1'
+gtPrefix = 'train3'
+trainPrefix = 'train3'
 gtDir = 'groundtruth/' + gtPrefix + '/'
 imagesDir = gtDir + 'images/'
 experimentDir = 'experiments/' + trainPrefix + '/'
@@ -230,13 +230,14 @@ dataIds = groundTruth['trainIds']
 
 gtDtype = groundTruth.dtype
 
-loadFromHdf5 = True
-if loadFromHdf5:
-    images = readImages(imagesDir, dataIds, loadFromHdf5)
+loadFromHdf5 = False
+
+images = readImages(imagesDir, dataIds, loadFromHdf5)
 
 print("Backprojecting and fitting estimates.")
 
-testSet = np.load(experimentDir + 'test.npy')[:20]
+# testSet = np.load(experimentDir + 'test.npy')[:20]
+testSet = np.arange(len(images))
 
 testAzsGT = dataAzsGT[testSet]
 testObjAzsGT = dataObjAzsGT[testSet]
@@ -332,8 +333,7 @@ testSamples = 1
 if recognitionType == 2:
     testSamples  = numSamples
 
-# for test_i in range(len(testAzsRel)):
-for test_i in [7]:
+for test_i in range(len(testAzsRel)):
 
     bestPredAz = chAz.r
     bestPredEl = chEl.r
@@ -348,16 +348,16 @@ for test_i in [7]:
 
     cv2.imwrite(resultDir + 'imgs/test'+ str(test_i) + '_id' + str(testId) +'_groundtruth' + '.png', cv2.cvtColor(np.uint8(rendererGT.r*255), cv2.COLOR_RGB2BGR))
 
-    chObjAz[:] = testObjAzsGT[test_i]
+    chObjAz[:] = 0
 
     for sample in range(testSamples):
         from numpy.random import choice
         if recognitionType == 0:
             #Prediction from (near) ground truth.
             color = testVColorGT[test_i] + nearGTOffsetVColor
-            az = chObjAz.r[:] + testAzsRel[test_i] + nearGTOffsetRelAz
+            az = testAzsRel[test_i] + nearGTOffsetRelAz
             el = testElevsGT[test_i] + nearGTOffsetEl
-            SHcomponents = testComponentsGT[test_i] + nearGTOffsetSHComponent
+            SHcomponents = testComponentsGTRel[test_i]
         elif recognitionType == 1:
             #Point (mean) estimate:
             az = azsPredRF[test_i][test_i]
@@ -380,7 +380,6 @@ for test_i in [7]:
         # chVColors[:] = testPredVColors[test_i]
         chComponent[:] = SHcomponents
 
-        ipdb.set_trace()
         cv2.imwrite(resultDir + 'imgs/samples/test'+ str(test_i) + '_sample' + str(sample) +  '_predicted'+ '.png', cv2.cvtColor(np.uint8(renderer.r*255), cv2.COLOR_RGB2BGR))
 
         ch.minimize({'raw': errorFun}, bounds=None, method=methods[method], x0=free_variables, callback=cb, options={'disp':False, 'maxiter':maxiter})

diffrender_train.py
@@ -25,12 +25,10 @@ import pickle
 seed = 1
 np.random.seed(seed)
 
-gtPrefix = 'test'
-trainPrefix = 'train1'
+gtPrefix = 'train3'
+trainPrefix = 'train3'
 gtDir = 'groundtruth/' + gtPrefix + '/'
 experimentDir = 'experiments/' + trainPrefix + '/'
-testPrefix = 'test1'
-resultDir = 'results/' + trainPrefix + '_' + testPrefix + '/'
 
 groundTruthFilename = gtDir + 'groundTruth.h5'
 gtDataFile = h5py.File(groundTruthFilename, 'r')
@@ -71,7 +69,7 @@ trainComponentsGTRel = dataElevsGT[trainSet]
 imagesDir = gtDir + 'images/'
 
 loadFromHdf5 = True
-writeHdf5 = False
+writeHdf5 = True
 
 if writeHdf5:
     writeImagesHdf5(imagesDir, dataIds)

utils.py
@@ -27,7 +27,7 @@ def readImages(imagesDir, imageSet, loadFromHdf5):
         image = skimage.io.imread(imagesDir + 'im' + str(imageSet[0]) + '.jpeg')
         width = image.shape[1]
         height = image.shape[0]
-        images = np.zeros([len(imageSet), height, width, 3], astype=np.float32)
+        images = np.zeros([len(imageSet), height, width, 3], dtype=np.float32)
         for imageit, imageid  in enumerate(imageSet):
             image = skimage.io.imread(imagesDir + 'im' + str(imageid) + '.jpeg')
             images[imageit, :, :, :] = image/255.0