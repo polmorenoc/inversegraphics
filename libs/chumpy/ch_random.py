4850cba96c74c9b0ed6cf71e8d831637a99fe0ee
polmorenoc <polmorenoc@gmail.com>
2016-12-22 15:47:24 +0000

    split opendr from this project

--- libs/chumpy/ch_random.py
+++
@@ -1,9 +0,0 @@
-"""
-Author(s): Matthew Loper
-
-See LICENCE.txt for licensing and contact information.
-"""
-
-import numpy.random
-from ch import Ch
-
--- libs/opendr/version.py
+++
@@ -1,3 +0,0 @@
-version = '0.73'
-short_version = version
-full_version = version
